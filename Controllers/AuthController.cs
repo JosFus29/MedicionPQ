6ad77b5b473d69d45e89e7d556e2a6196591f69f
JosFus29 <joshuafaridmejia@gmail.com>
2026-09-21 10:00:05 -0600

    Implementamos programacion asincrona para atender varios usuarios a la vez

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+// se agrega para poder usar Entity Framework Core
+using Microsoft.EntityFrameworkCore;
 using MedicionPQ.Modelos;
 using MedicionPQ.Data;
 using Microsoft.AspNetCore.HttpLogging;
@@ -16,10 +18,10 @@ public class AuthController : ControllerBase
     {
         _context = context;
     }
-    
 
+    // agregamos async y task para que sea asincrono y no bloquee el hilo principal
     [HttpPost("login")]
-    public IActionResult Login([FromBody] LoginRequest request)
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
     {
         //validamos que no vengan vacios los campos
         if (string.IsNullOrEmpty(request.Correo) || string.IsNullOrEmpty(request.Contrasena))
@@ -28,32 +30,30 @@ public class AuthController : ControllerBase
         }
 
         // buscamos en la tabla de usuarios si existe el usuario con la cuenta y clave proporcionados
-        var usuarioEnDb = _context.Usuarios.
-            FirstOrDefault(u => u.correo == request.Correo && u.contrasena == request.Contrasena);
+        var usuarioEnDb = await _context.Usuarios.
+            FirstOrDefaultAsync(u => u.correo == request.Correo && u.contrasena == request.Contrasena);
 
         //si se encuentra el correo y la contraseña es correcta, retornamos un mensaje de éxito
         if (usuarioEnDb != null)
         {
+            // Validar si el usuario está activo (bit edo = 1)
             if (!usuarioEnDb.edo)
             {
                 return Unauthorized(new { message = "Usuario inactivo" });
             }
-
-            return Ok(new { 
-                
-                message = "Inicio de sesión exitoso", 
+            return Ok(new
+            {
+                message = "Inicio de sesión exitoso",
                 usuario = usuarioEnDb.idUsuario,
                 correo = usuarioEnDb.correo,
                 nombreUsuario = usuarioEnDb.nombreUsuario,
-                //probamos que el rol se pueda enviar como string
+                //probamos que el rol se pueda enviar como string (si lo mandamos como entero, el cliente no lo va a entender)
                 rol = usuarioEnDb.rol.ToString()
 
             });
+
         }
-        else
-        {
             //si no se encuentra el usuario o la clave es incorrecta, retornamos un mensaje de error
             return Unauthorized(new { message = "Correo o Contraseña incorrectos" });
-        }
     }
 }
